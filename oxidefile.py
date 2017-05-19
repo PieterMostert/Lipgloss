eac071214323bd65efac0dcd6d7f5ed68093ca7c
Pieter Mostert <pi.mostert@gmail.com>
2017-05-19 09:44:10 +0200

    Changed relative atomic masses to CRC values

--- oxidefile.py
+++ oxidefile.py
@@ -17,11 +17,11 @@
 # Contact: pi.mostert@gmail.com
 
 # Initialize oxides:
-oxides = ('SiO2', 'Al2O3', 'B2O3', 'MgO', 'CaO', 'SrO', 'BaO', 'ZnO', 'Li2O', 'Na2O', 'K2O', 'P2O5',
-          'Fe2O3', 'TiO2', 'MnO2')
+oxides = ('SiO2', 'Al2O3', 'B2O3', 'MgO', 'CaO', 'SrO', 'BaO', 'ZnO', 'Li2O',
+          'Na2O', 'K2O', 'P2O5', 'Fe2O3', 'TiO2', 'MnO2')
 
-molar_masses = (60.083, 101.961, 69.617, 40.304, 56.077, 103.619, 153.329, 81.379, 29.879, 61.979, 94.191,
-                141.943, 159.687, 79.865, 86.937) 
+molar_masses = (60.085, 101.961, 69.62, 40.304, 56.077, 103.62, 153.326, 81.39, 29.881,
+                61.979, 94.196, 141.945, 159.688, 79.866, 86.937) 
 molar_mass_dict = {oxides[i]: molar_masses[i] for i in range(len(oxides))}
 
 fluxes = ('Li2O', 'Na2O', 'MgO', 'K2O', 'CaO', 'ZnO', 'SrO', 'BaO') 
